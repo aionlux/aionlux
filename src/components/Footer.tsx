42b7601cf27844b5199baf8365eca1c799aa8590
addleonel <adleonel3633@gmail.com>
2023-09-12 23:08:45 -0500

    new appearance

---
+++ src/components/Footer.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+import "../assets/styles/Footer.scss";
+
+const Footer: React.FC = () => {
+  return (
+    <React.Fragment>
+      <div className="footer-container">
+        <div className="home-footer">
+          <ul className="home-footer__ul">
+            <li className="home-footer__li">
+              <Link to="/about/" className="home-footer__link">
+                _Nosotros
+              </Link>
+            </li>
+            <li className="home-footer__li">
+              <Link to="/contact/" className="home-footer__link">
+                _Contacto
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <p className="home-footer__copyright">
+          2023 &#169; AILOOK - Todos los derechos reservados
+        </p>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Footer;
