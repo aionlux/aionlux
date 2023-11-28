546223c0bdf5146feca602b91cee5b3b5dd0e9b3
addleonel <adleonel3633@gmail.com>
2023-11-27 23:22:22 -0500

    New video background, no _AVANCE button, and youtube video added

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -13,8 +13,7 @@ const Main: React.FC= () => {
     height: '390',
     width: '690',
     playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-      autoplay: 1,
+      autoplay: 0,
     },
   };
   return (
@@ -22,29 +21,26 @@ const Main: React.FC= () => {
           <div className="wrap">
             <Header />
             <div className="main">
-              <section className="section-m section-1">
-                <video className="video-background" controls={false} autoPlay muted loop>
-                  <source src="https://aionlux.com/videos/bg-2.mp4" type="video/mp4"/>
-                </video>
-                <div className="section-1-content">
-                  <div className="portal-phrase">
-                    <h1 className="portal-title">
-                    Sistema integrado de identificación, 
-                    control y monitoreo de vehículos impulsada por <span className="portal-title-span">inteligencia artificial.</span>
-                    </h1>
-                    <p className="portal-subtitle">
-                    Con aplicaciones en seguridad vial, gestión de tránsito y estacionamientos.
-                    </p>
-                    <button
-                      className="principal__message-button"
-                      onClick={handleShow}
-                    >
-                      _avances
-                    </button>
-                    {/* <YouTube videoId="J9t-6mN1FAw" opts={opts} /> */}
+                <section className="section-m section-1">
+                  <video className="video-background" controls={false} autoPlay muted loop>
+                    <source src="https://aionlux.com/videos/cars.mp4" type="video/mp4"/>
+                  </video>
+                  <div className="section-1-content">
+                      <div className="portal-phrase">
+                    <Container fluid="xl">
+                        <h1 className="portal-title">
+                        Sistema integrado de identificación, 
+                        control y monitoreo de vehículos impulsada por <span className="portal-title-span">inteligencia artificial.</span>
+                        </h1>
+                        <p className="portal-subtitle">
+                        Con aplicaciones en seguridad vial, gestión de tránsito y estacionamientos.
+                        </p>
+                      
+                        <YouTube className={"portal-video"} videoId="lgmiZVk2vzQ" opts={opts} />
+                    </Container>
+                      </div>
                   </div>
-                </div>
-              </section>
+                </section>
             </div>
           </div>
           <div className="footer">
